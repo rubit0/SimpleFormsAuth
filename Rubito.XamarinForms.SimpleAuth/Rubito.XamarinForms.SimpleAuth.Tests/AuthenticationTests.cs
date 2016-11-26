4f1739b50ffa46ef99eb2dc47aa801b9081674bf
Ruben <Ruben@studio-delatorre.com>
2016-11-26 21:17:52 +0100

    Added Unit Test project.

---
+++ Rubito.XamarinForms.SimpleAuth/Rubito.XamarinForms.SimpleAuth.Tests/AuthenticationTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rubito.XamarinForms.SimpleAuth.Models;
+using System;
+
+namespace Rubito.XamarinForms.SimpleAuth.Tests
+{
+    [TestClass]
+    public class AuthenticationTests
+    {
+        [TestMethod]
+        public void TokenIsInvalidOnExpiredDate()
+        {
+            var token = new BearerToken("TOKEN", "User", DateTime.Now, (DateTime.Now - new TimeSpan(1, 0, 0, 0)));
+
+            Assert.IsTrue(token.IsExpired());
+        }
+    }
+}
