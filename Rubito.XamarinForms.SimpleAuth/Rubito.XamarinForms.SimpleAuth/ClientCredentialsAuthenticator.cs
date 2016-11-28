bbf0629eaca55c979313a6af8d7330f07b0d0659
Ruben <Ruben@studio-delatorre.com>
2016-11-29 00:24:50 +0100

    Moving implementation to XamarinAuth. [WIP]

--- Rubito.XamarinForms.SimpleAuth/Rubito.XamarinForms.SimpleAuth/ClientCredentialsAuthenticator.cs
+++ Rubito.XamarinForms.SimpleAuth/Rubito.XamarinForms.SimpleAuth/ClientCredentialsAuthenticator.cs
@@ -1,69 +1,105 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using ModernHttpClient;
 using Rubito.XamarinForms.SimpleAuth.Models;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Xamarin.Auth;
+using Xamarin.Utilities;
 
 namespace Rubito.XamarinForms.SimpleAuth
 {
-    public class ClientCredentialsAuthenticator
+    public class ClientCredentialsAuthenticator : FormAuthenticator
     {
-        public Uri TokenEndpoint { get; }
-        private readonly Credentials _credentials;
+        bool _usernameIsEmail;
+        Credentials _credentials;
+        Uri _accessTokenUrl;
 
-        public ClientCredentialsAuthenticator(Uri tokenEndpoint, Credentials credentials)
+        Uri AccessTokenUrl
         {
-            if (tokenEndpoint == null || credentials == null)
-                throw new ArgumentNullException();
-
-            TokenEndpoint = tokenEndpoint;
-            _credentials = credentials;
+            get { return this._accessTokenUrl; }
         }
 
-        public ClientCredentialsAuthenticator(Uri tokenEndpoint, string username, string password)
+        public ClientCredentialsAuthenticator(Uri accessTokenUrl, Credentials credentials) : base(accessTokenUrl)
         {
-            var credentials = new Credentials(username, password);
+            if (accessTokenUrl == null)
+                throw new ArgumentNullException(nameof(accessTokenUrl), "an accessTokenUrl must be provided");
 
-            if (tokenEndpoint == null || credentials == null)
-                throw new ArgumentNullException();
+            if (credentials == null)
+                throw new ArgumentNullException(nameof(credentials), "credentials must be provided");
 
-            TokenEndpoint = tokenEndpoint;
-            _credentials = credentials;
+            this.InitializeFormFields();
+            this._accessTokenUrl = accessTokenUrl;
+            this._credentials = credentials;
         }
 
-        public async Task<AuthenticationResult> AuthenticateAsync(HttpMessageHandler httpMessageHandler = null)
+        public ClientCredentialsAuthenticator(Uri accessTokenUrl, string username, string password)
         {
-            var client = (httpMessageHandler != null) ? new HttpClient(httpMessageHandler) : new HttpClient();
+            if (accessTokenUrl == null)
+                throw new ArgumentNullException(nameof(accessTokenUrl), "an accessTokenUrl must be provided");
+
+            if (string.IsNullOrEmpty(username))
+                throw new ArgumentNullException(nameof(username), "an username must be provided");
+
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password), "an password must be provided");
+
+            this.InitializeFormFields();
+            this._accessTokenUrl = accessTokenUrl;
+            this._credentials = new Credentials(username, password);
+        }
+
+        public override async Task<Account> SignInAsync(CancellationToken cancellationToken)
+        {
+            var result = await AuthenticateAsync();
+            return new Account(result["user"], result);
+        }
+
+        protected async Task<IDictionary<string, string>> AuthenticateAsync()
+        {
+            var query = _credentials.ToFormUrlEncodedContent();
+            var client = new HttpClient(new NativeMessageHandler());
+
             using (client)
             {
-                var response = await client.PostAsync(TokenEndpoint, _credentials.ToFormUrlEncodedContent());
-                var result = await HttpResponseValidationAsync(response);
+                var response = await client.PostAsync(_accessTokenUrl, query);
+                var contentStream = await response.Content.ReadAsStringAsync();
+                var data = contentStream.Contains("{")
+                    ? WebEx.JsonDecode(contentStream)
+                    : WebEx.FormDecode(contentStream);
 
-                return result;
+                if (data.ContainsKey("error"))
+                    throw new AuthException($"Error authenticating: {data["error"]}");
+                else if (data.ContainsKey("access_token"))
+                    return data;
+                else
+                    throw new AuthException("Expected an access_token in the response.");
             }
         }
 
-        private async Task<AuthenticationResult> HttpResponseValidationAsync(HttpResponseMessage response)
+        protected void InitializeFormFields()
         {
-            var contentStream = await response.Content.ReadAsStringAsync();
-
-            if (response.IsSuccessStatusCode)
+            FormAuthenticatorField username = new FormAuthenticatorField
             {
-                var token = await Task.Factory
-                    .StartNew(() => JsonConvert.DeserializeObject<BearerToken>(contentStream));
+                FieldType = (_usernameIsEmail)
+                    ? FormAuthenticatorFieldType.Email
+                    : FormAuthenticatorFieldType.PlainText,
+                Key = "username",
+                Title = "Username",
+                Placeholder = "Username"
+            };
 
-                return new AuthenticationResult(true, "Success", token);
-            }
+            FormAuthenticatorField password = new FormAuthenticatorField
+            {
+                FieldType = FormAuthenticatorFieldType.Password,
+                Key = "password",
+                Title = "Password",
+                Placeholder = "Password"
+            };
 
-            var message = await Task.Factory
-                .StartNew(() => JsonConvert.DeserializeObject<JObject>(contentStream));
-
-            var errorMessage = "Error";
-            if (message["error_description"] != null)
-                errorMessage = (string)message["error_description"];
-
-            return new AuthenticationResult(false, errorMessage);
+            Fields.Add(username);
+            Fields.Add(password);
         }
     }
 }
--- Rubito.XamarinForms.SimpleAuth/Rubito.XamarinForms.SimpleAuth/Models/Credentials.cs
+++ Rubito.XamarinForms.SimpleAuth/Rubito.XamarinForms.SimpleAuth/Models/Credentials.cs
@@ -1,37 +1,69 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.RegularExpressions;
+using Xamarin.Auth;
 
 namespace Rubito.XamarinForms.SimpleAuth.Models
 {
     public class Credentials
     {
-        public readonly string Username;
-        public readonly string Password;
-        public readonly string Scope;
+        FormAuthenticatorField _username = new FormAuthenticatorField
+        {
+            FieldType = FormAuthenticatorFieldType.PlainText,
+            Key = "username",
+            Title = "Username",
+            Placeholder = "Username"
+        };
+
+        FormAuthenticatorField _password = new FormAuthenticatorField
+        {
+            FieldType = FormAuthenticatorFieldType.Password,
+            Key = "password",
+            Title = "Password",
+            Placeholder = "Password"
+        };
+
+        public FormAuthenticatorField Username
+        {
+            get { return this._username; }
+        }
+
+        public FormAuthenticatorField Password
+        {
+            get { return this._password; }
+        }
+
         public bool RememberMe { get; set; }
         public bool StorePassword { get; set; }
 
-        public Credentials(string username, string password, string scope = null)
+        public Credentials(string username, string password)
         {
-            Username = username;
-            Password = password;
-            Scope = scope;
+            if (string.IsNullOrEmpty(username))
+                throw new ArgumentNullException(nameof(username), "you must provide a username.");
+
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password), "you must provide a username.");
+
+            this._username.Value = username;
+            this._password.Value = password;
         }
 
-        public FormUrlEncodedContent ToFormUrlEncodedContent()
+        public IDictionary<string, string> ToQuerryValues()
         {
             var credentialsEncoded = new Dictionary<string, string>
             {
                 {"grant_type", "password"},
-                {"username", this.Username},
-                {"password", this.Password}
+                {"username", this.Username.Value},
+                {"password", this.Password.Value}
             };
 
-            if (!string.IsNullOrEmpty(this.Scope))
-                credentialsEncoded.Add("scope", this.Scope);
+            return credentialsEncoded;
+        }
 
-            return new FormUrlEncodedContent(credentialsEncoded);
+        public FormUrlEncodedContent ToFormUrlEncodedContent()
+        {
+            return new FormUrlEncodedContent(ToQuerryValues());
         }
 
         public CredentialsValidationResult Validate(uint minUsernameLength = 4, uint minPasswordLength = 6, bool isEmail = false)
@@ -39,34 +71,34 @@ namespace Rubito.XamarinForms.SimpleAuth.Models
             List<string> messages = new List<string>();
             var isValid = true;
 
-            if (string.IsNullOrEmpty(Username))
+            if (string.IsNullOrEmpty(Username.Value))
             {
                 messages.Add("Enter your Username.");
                 isValid = false;
             }
             else
             {
-                if (isEmail && !IsValidEmail(Username))
+                if (isEmail && !IsValidEmail(Username.Value))
                 {
                     messages.Add("Invalid email address.");
                     isValid = false;
                 }
 
-                if (Username.Length < minUsernameLength)
+                if (Username.Value.Length < minUsernameLength)
                 {
                     messages.Add($"Username must be {minUsernameLength} characters long.");
                     isValid = false;
                 }
             }
 
-            if (string.IsNullOrEmpty(Password))
+            if (string.IsNullOrEmpty(Password.Value))
             {
                 messages.Add("Enter a Password.");
                 isValid = false;
             }
             else
             {
-                if (Password.Length < minPasswordLength)
+                if (Password.Value.Length < minPasswordLength)
                 {
                     messages.Add($"Password must be {minPasswordLength} characters long.");
                     isValid = false;
@@ -78,6 +110,7 @@ namespace Rubito.XamarinForms.SimpleAuth.Models
 
         private bool IsValidEmail(string input)
         {
+            //http://stackoverflow.com/a/16168103
             var result = Regex.IsMatch(input, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
             return result;
         }
